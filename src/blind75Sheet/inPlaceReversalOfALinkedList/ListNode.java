0a0771722cdda4a9463e91c2dcdf9b23c8eda921
Nisha Tiwari <89537080+nishtiw@users.noreply.github.com>
2025-06-29 17:51:24 +0530

    Solution for Q206 LC

---
+++ src/blind75Sheet/inPlaceReversalOfALinkedList/ListNode.java
@@ -0,0 +1,37 @@
+package blind75Sheet.inPlaceReversalOfALinkedList;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode curr = this;
+        sb.append("[");
+        while (curr != null) {
+            sb.append(curr.val);
+            if (curr.next != null) {
+                sb.append(", ");
+            }
+            curr = curr.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
