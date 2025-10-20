9b86c3163c74f2a6454717fbb588dfd0c63b5593
Nisha Tiwari <89537080+nishtiw@users.noreply.github.com>
2025-10-20 22:52:02 +0530

    Refactored code for NonOverlappingIntervaks and ReverseLinkedList

--- src/blind75Sheet/inPlaceReversalOfALinkedList/ReverseLinkedList.java
+++ src/blind75Sheet/inPlaceReversalOfALinkedList/ReverseLinkedList.java
@@ -3,53 +3,23 @@ package blind75Sheet.inPlaceReversalOfALinkedList;
 
 import blind75Sheet.utils.inPlaceReversalOfALinkedList.ListNode;
 
+import static blind75Sheet.utils.inPlaceReversalOfALinkedList.ListUtils.createList;
+
 public class ReverseLinkedList {
     public static void main(String[] args) {
         ReverseLinkedList obj = new ReverseLinkedList();
 
         // Test 1: head = [1,2,3,4,5]
-        ListNode head1 = obj.createList(new int[]{1, 2, 3, 4, 5}, -1);
-        System.out.print("Test 1: [1,2,3,4,5] → " + reverseList(head1)); // Output : [5, 4, 3, 2, 1]
+        ListNode head1 = createList(new int[]{1, 2, 3, 4, 5}, -1);
+        System.out.print("Test 1: [1,2,3,4,5] → " + obj.reverseList(head1)); // Output : [5, 4, 3, 2, 1]
 
         // Test 2: head = [1,2]
-        ListNode head2 = obj.createList(new int[]{1, 2}, -1);
-        System.out.print("\nTest 2: [1,2] → " + reverseList(head2)); // Output : [2, 1]
+        ListNode head2 = createList(new int[]{1, 2}, -1);
+        System.out.print("\nTest 2: [1,2] → " + obj.reverseList(head2)); // Output : [2, 1]
 
         // Test 3: head = []
-        ListNode head3 = obj.createList(new int[]{}, -1);
-        System.out.print("\nTest 3: [] → " + (reverseList(head3) == null ? "[]" : reverseList(head3))); // Output : []
-    }
-
-    /**
-     * Create list with cycle at position 'pos'.
-     * <p>
-     * This method is not a part of the solution.
-     * It is only used for preparing test cases.
-     *
-     * @param values
-     * @param pos
-     * @return ListNode
-     */
-    public ListNode createList(int[] values, int pos) {
-        if (values == null || values.length == 0) return null;
-
-        ListNode head = new ListNode(values[0]);
-        ListNode current = head;
-        ListNode cycleEntry = null;
-
-        if (pos == 0) cycleEntry = head;
-
-        for (int i = 1; i < values.length; i++) {
-            current.next = new ListNode(values[i]);
-            current = current.next;
-            if (i == pos) cycleEntry = current;
-        }
-
-        if (pos != -1) {
-            current.next = cycleEntry;  // Create cycle
-        }
-
-        return head;
+        ListNode head3 = createList(new int[]{}, -1);
+        System.out.print("\nTest 3: [] → " + (obj.reverseList(head3) == null ? "[]" : obj.reverseList(head3))); // Output : []
     }
 
     /**
@@ -59,7 +29,7 @@ public class ReverseLinkedList {
      * @param head The head of the singly linked list.
      * @return The new head of the reversed linked list.
      */
-    public static ListNode reverseList(ListNode head) {
+    public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
 
