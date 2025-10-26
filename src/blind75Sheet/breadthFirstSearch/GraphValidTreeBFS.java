eab449c9d4740be3d92beb07bb7ef99675440ade
Nisha Tiwari <89537080+nishtiw@users.noreply.github.com>
2025-10-26 21:56:09 +0530

    Solution for LC Q261 and GFG Graph is Tree or Not - BFS and DFS solutions

---
+++ src/blind75Sheet/breadthFirstSearch/GraphValidTreeBFS.java
@@ -0,0 +1,96 @@
+package blind75Sheet.breadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * BFS Solution
+ * Approach inspired by Akshay Anil, reference = https://www.youtube.com/watch?v=NBSXzs8mrOQ
+ */
+public class GraphValidTreeBFS {
+    public static void main(String[] args) {
+        GraphValidTreeBFS obj = new GraphValidTreeBFS();
+
+        // Test case 1: n1 = 4, m1 = 3, edges1 = [[0, 1], [1, 2], [1, 3]]
+        int n1 = 4, m1 = 3;
+        ArrayList<ArrayList<Integer>> edges1 = new ArrayList<>();
+        edges1.add(new ArrayList<>(Arrays.asList(0, 1)));
+        edges1.add(new ArrayList<>(Arrays.asList(1, 2)));
+        edges1.add(new ArrayList<>(Arrays.asList(1, 3)));
+        boolean result1 = obj.isTree(n1, m1, edges1); // true
+        System.out.println("n1=" + n1 + ", m1=" + m1 + ", edges1=" + edges1 + " => " + result1);
+
+        // Test case 1: n1 = 4, m1 = 3, edges1 = [[0, 1], [1, 2], [2, 0]]
+        int n2 = 4, m2 = 3;
+        ArrayList<ArrayList<Integer>> edges2 = new ArrayList<>();
+        edges2.add(new ArrayList<>(Arrays.asList(0, 1)));
+        edges2.add(new ArrayList<>(Arrays.asList(1, 2)));
+        edges2.add(new ArrayList<>(Arrays.asList(2, 0)));
+        boolean result2 = obj.isTree(n2, m2, edges2); // false
+        System.out.println("n2=" + n2 + ", m2=" + m2 + ", edges2=" + edges2 + " => " + result2);
+
+    }
+
+    /**
+     * Checks if the Graph is a Tree.
+     * <p>
+     * TC: O(n + m)
+     * SC: O(n)
+     *
+     * @param n     nodes (numbered from 0 to N-1)
+     * @param m     edges
+     * @param edges
+     * @return
+     */
+    public boolean isTree(int n, int m, ArrayList<ArrayList<Integer>> edges) {
+        // A graph with 'n' nodes and 'm' edges is a tree only if m == n - 1
+        if (m != n - 1) {
+            return false;
+        }
+
+        // Initialize visited array
+        boolean[] visited = new boolean[n];
+
+        // Initialize adjacency list
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Build the adjacency list (assuming an undirected graph)
+        for (int i = 0; i < m; i++) {
+            adj.get(edges.get(i).get(0)).add(edges.get(i).get(1));
+            adj.get(edges.get(i).get(1)).add(edges.get(i).get(0));
+        }
+
+        // Initialize the queue for BFS
+        Queue<Integer> queue = new LinkedList<>();
+
+        // 'nodeCount' tracks the number of nodes reached in the BFS
+        int nodeCount = 1;
+
+        // 'startNode' is the starting node for the BFS (assuming node 0)
+        int startNode = 0;
+
+        // Start BFS from startNode
+        visited[startNode] = true;
+        queue.add(startNode);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int neighbor : adj.get(current)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    nodeCount++;
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        // A connected graph with n nodes and n-1 edges is a tree.
+        // We check if all 'n' nodes were reached (i.e., the graph is connected).
+        return nodeCount == n;
+    }
+}
