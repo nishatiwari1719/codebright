2134a4b34eb2426f74f4c505361ce43271215f29
Nisha Tiwari <89537080+nishtiw@users.noreply.github.com>
2026-06-20 18:32:05 +0530

    Improved some solutions

--- src/blind75Sheet/btiwiseXOR/NumberOf1Bits.java
+++ src/blind75Sheet/btiwiseXOR/NumberOf1Bits.java
@@ -21,6 +21,29 @@ public class NumberOf1Bits {
     }
 
     /**
+     * Counts the number of set bits (1s) in the binary representation
+     * of a 32-bit integer using Brian Kernighan's algorithm.
+     * <p>
+     * Each iteration removes the lowest set bit:
+     * n = n & (n - 1)
+     * <p>
+     * The loop runs only as many times as there are set bits.
+     *
+     * @param n the input integer
+     * @return number of set bits
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+
+        return count;
+    }
+
+    /**
      * Counts the number of set bits (1-bits) in the binary representation
      * of a 32-bit integer.
      *
@@ -36,18 +59,18 @@ public class NumberOf1Bits {
      * @param n the integer whose set bits are to be counted
      * @return the number of bits set to 1 in the binary representation of {@code n}
      */
-    public int hammingWeight(int n) {
-        int count = 0;
-
-        for (int i = 0; i < 32; i++) {
-            if ((n & 1) == 1) {
-                count++;
-            }
-            n = n >> 1;
-        }
-
-        return count;
-    }
+//    public int hammingWeight(int n) {
+//        int count = 0;
+//
+//        for (int i = 0; i < 32; i++) {
+//            if ((n & 1) == 1) {
+//                count++;
+//            }
+//            n = n >> 1;
+//        }
+//
+//        return count;
+//    }
 }
 
 /**
